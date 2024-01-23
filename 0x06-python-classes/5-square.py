4cc6ad8a7011c5f04f2fb0d070a3dc82af6a33c7
BSM-777 <marouanebassim12@gmail.com>
2024-01-23 21:22:36 +0100

    New

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+class Square:
+    def __init__(self, value ):
+        if type(value) is int:
+            if value < 0:
+                raise ValueError('size must be >= 0')
+            else:
+                self.__size = value
+        else:
+            raise TypeError('size must be an integer')
+        
+    def area(self):
+        return (self.__size) ** 2
+
+    def size(self):
+        return self.__size
+    
+    def __init__(self, size = 0):
+        self.__size = size
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+            return
+        for i in range(self.__size):
+            print("".join(["#" for j in range(self.__size)]))
