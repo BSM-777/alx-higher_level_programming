44bd4d69f272c857107f9ace0aafb4da50e23853
BSM-777 <marouanebassim12@gmail.com>
2024-01-23 21:32:39 +0100

    New

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
+""" defines a square """
+
 
 class Square:
-    def __init__(self, size = 0 ):
+    """ square with private instance attribute size """
+
+    def __init__(self, size=0):
+        """
+        initializes square
+        Args:
+            size: size of side of square
+        """
+
         if type(size) is int:
             if size < 0:
                 raise ValueError('size must be >= 0')
@@ -9,6 +19,12 @@ class Square:
                 self.__size = size
         else:
             raise TypeError('size must be an integer')
-        
+
     def area(self):
+        """
+        finds area of square
+        Returns:
+            the area of the square
+        """
+
         return self.__size ** 2
